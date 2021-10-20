84e8b53bdf7e180a1ddc7a91fc1c276a7b138371
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2021-10-21 01:57:08 +0300

    add exercises

--- JSAdvanced/Exam - 27 June 2020/03.VeterinaryClinic/03.VeterinaryClinic.js
+++ JSAdvanced/Exam - 27 June 2020/03.VeterinaryClinic/03.VeterinaryClinic.js
@@ -8,25 +8,82 @@ class VeterinaryClinic {
     }
 
     newCustomer(ownerName, petName, kind, procedures) {
-        let currPet = this.clients.find(x => x.petName == petName);
+        if (this.currentWorkload < this.capacity) {
+            let findClient = this.clients.find(x => x[ownerName] != undefined);
 
-        if (currPet == undefined) {
-            petName = {
-                ownerName,
-                kind,
-                procedures
-            };
+            if (findClient == undefined) {
+                findClient = {};
+                findClient[ownerName] = {};
+                findClient[ownerName][petName] = { kind, procedures };
+                this.clients.push(findClient);
+            }
+            else {
+                if (findClient[ownerName][petName] != undefined && findClient[ownerName][petName].procedures.length > 0) {
+                    throw new Error(`This pet is already registered under ${ownerName} name! ${petName} is on our lists, waiting for ${findClient[ownerName][petName].procedures.join(', ')}.`);
+                }
+                else {
+                    findClient[ownerName][petName] = { kind, procedures };
+                }
+            }
 
-            this.clients.push({ petName });
+            this.currentWorkload++;
+            return `Welcome ${petName}!`;
         }
+
+        throw new Error(`Sorry, we are not able to accept more patients!`);
+    }
+
+    onLeaving(ownerName, petName) {
+        let findClient = this.clients.find(x => x[ownerName] != undefined);
+
+        if (findClient == undefined) {
+            throw new Error('Sorry, there is no such client!');
+        }
+
+        if (findClient[ownerName][petName] == undefined || findClient[ownerName][petName].procedures.length == 0) {
+            throw new Error(`Sorry, there are no procedures for ${petName}!`);
+        }
+
+        this.totalProfit += 500 * findClient[ownerName][petName].procedures.length;
+        this.currentWorkload--;
+        findClient[ownerName][petName].procedures.length = 0;
+
+        return `Goodbye ${petName}. Stay safe!`;
+    }
+
+    toString() {
+        let result = `${this.clinicName} is ${Math.floor(this.currentWorkload / this.capacity * 100)}% busy today!` + '\n' +
+            `Total profit: ${this.totalProfit.toFixed(2)}$`;
+
+        for (const client of this.clients.sort((a, b) => Object.keys(a)[0].localeCompare(Object.keys(b)[0]))) {
+            result += '\n' + `${Object.keys(client)} with:`;
+
+            for (const pets of Object.values(client)) {
+
+                for (const pet of Object.entries(pets).sort((a, b) => a[0].localeCompare(b[0]))) {
+                    const temp = pet;
+                    result += '\n' + `---${pet[0]} - a ${pet[1].kind.toLowerCase()} that needs: ${pet[1].procedures.join(', ')}`;
+                }
+            }
+        }
+
+        return result;
     }
 }
 
 let clinic = new VeterinaryClinic('SoftCare', 10);
-clinic.newCustomer('Jim Jones', 'Tom', 'Cat', ['A154B', '2C32B', '12CDB']);
-clinic.newCustomer('Anna Morgan', 'Max', 'Dog', ['SK456', 'DFG45', 'KS456']);
-console.log(clinic.newCustomer('Jim Jones', 'Tiny', 'Cat', ['A154B']));
-console.log(clinic.onLeaving('Jim Jones', 'Tiny'));
+console.log(clinic.newCustomer('Jim Jones', 'Tom', 'Cat', ['A154B', '2C32B', '12CDB']));
+console.log(clinic.newCustomer('Anna Morgan', 'Max', 'Dog', ['SK456', 'DFG45', 'KS456']));
+console.log(clinic.newCustomer('Jim Jones', 'Tiny', 'Cat', ['A154B']));
+console.log(clinic.onLeaving('Jim Jones', 'Tiny'));
 console.log(clinic.toString());
-clinic.newCustomer('Jim Jones', 'Sara', 'Dog', ['A154B']);
+clinic.newCustomer('Jim Jones', 'Sara', 'Dog', ['A154B']);
 console.log(clinic.toString());
+
+
+
+
+
+
+
+
